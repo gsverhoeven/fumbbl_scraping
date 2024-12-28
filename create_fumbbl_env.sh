b7a0134c8a42db6959ff716de37a5979753baace
Gertjan <gertjan.verhoeven@gmail.com>
2024-12-28 19:47:48 +0100

    keep track of package dependencies

---
+++ create_fumbbl_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m venv fumbbl_env
+source fumbbl_env/bin/activate
+pip install pandas
+pip install jupyter
+pip install plotnine
+pip install isoweek
+pip install lxml
+pip install faust-cchardet
+pip install tables
