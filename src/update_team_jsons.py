1d398b0cc2c6adb266dfa44bf1b5e3854a1751eb
Gertjan <gertjan.verhoeven@gmail.com>
2025-01-07 00:01:56 +0100

    get function up and running

--- src/update_team_jsons.py
+++ src/update_team_jsons.py
@@ -1,9 +1,8 @@
 import gzip
+import json
 import os
 import requests
 import time
-from src.write_json_file import write_json_file
-
 
 def update_team_jsons(full_run, teamslist, verbose = False):
 
@@ -27,27 +26,51 @@ def update_team_jsons(full_run, teamslist, verbose = False):
                 os.makedirs(dirname)
 
             fname_string = dirname + "/team_" + str(team_id) + ".json" + ".gz"
-
+            fname_string_short = dirname + "/team_" + str(team_id) + ".json"
             # check if file already exists, else scrape it
             try:
                 f = open(fname_string, mode = "rb")
 
             except OSError as e:
-              # scrape it
-                api_string = "https://fumbbl.com/api/team/get/" + str(team_id)
+                try:
+                    f = open(fname_string_short, mode = "rb")
+                
+                except OSError as e:
+                # scrape it
+                    api_string = "https://fumbbl.com/api/team/get/" + str(team_id)
 
-                team = requests.get(api_string)
-                team = team.json()
+                    team = requests.get(api_string)
+                    team = team.json()
 
-                with gzip.open(fname_string, mode = "wb") as f:
-                    f.write(response.text.encode("utf-8"))
-                    f.close()
-                if verbose:
-                    print("x", end = '')
-                time.sleep(0.3)
+                    with gzip.open(fname_string, mode = "wb") as f:
+                        f.write(json.dumps(team).encode('utf-8'))
+                        f.close()
+                    if verbose:
+                        print("x", end = '')
+                    time.sleep(0.3)
+                else:
+                    # file already present
+                    if verbose:
+                        print("O", end = '')
+                    continue 
             else:
                 # file already present
                 if verbose:
                     print("o", end = '')
                 continue # continue forces the loop to start at the next iteration, 
             #whereas pass means, “there is no code to execute here,” and it will continue through the remainder of the loop body
+
+
+        #     # also get tournament skills via getoptions
+        # api_string = "https://fumbbl.com/api/team/getOptions/" + str(team_id)
+        # response = requests.get(api_string)
+        # response = response.json()
+        # response['tournamentSkills'] = json.loads(response['tournamentSkills'])
+
+        # fname_string = dirname + "/team_" + str(team_id) + "_skills.json.gz"
+        
+        # with gzip.open(fname_string, mode = "wb") as f:
+        #     f.write(json.dumps(response).encode("utf-8"))
+        #     f.close()
+        # time.sleep(0.3)
+        # https://github.com/gsverhoeven/fumbbl_datasets/blob/91d9fdfbef3250705a36c5cfba688cf6b858420f/fumbbl_dataset.ipynb            
